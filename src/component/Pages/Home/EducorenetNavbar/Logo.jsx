aa96c2d5b9ca2527bb50165e67f4ba79dd1efbe9
Bankole Olaniyi Sunday <adeniyisunday2244@gmail.com>
2024-11-09 15:59:33 +0100

    just adding some changes in the aspect of styling and also chaning the favicon icons

---
+++ src/component/Pages/Home/EducorenetNavbar/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+      this is the navbar logo component
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/component/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from './EducorenetNavbar/Logo'
+import Navmenu from './EducorenetNavbar/Navmenu'
+import NavButton from './EducorenetNavbar/NavButton'
+
+function Home() {
+  return (
+    <div>
+      this is the main component
+
+      <Logo />
+      <Navmenu />
+      <NavButton />
+
+    </div>
+  )
+}
+
+export default Home
